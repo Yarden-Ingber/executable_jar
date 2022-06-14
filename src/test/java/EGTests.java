589ec3d1113ea11bda0356efd875967b1f009265
Yarden.Ingber <yarden.ingber@applitools.com>
2022-06-14 16:12:01 +0300

    add changes

--- src/test/java/EGTests.java
+++ src/test/java/EGTests.java
@@ -7,6 +7,7 @@ import com.applitools.eyes.selenium.BrowserType;
 import com.applitools.eyes.selenium.ClassicRunner;
 import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.selenium.fluent.Target;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import io.cloudbeat.testng.CbTestNGListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,40 +20,35 @@ import java.net.URL;
 import java.util.UUID;
 
 @Listeners(CbTestNGListener.class)
-public class EGTests { //extends CbTestNg {
+public class EGTests {
 
     private static final String appName = "Execution grid";
     private static final String testName = "Execution grid tests";
     private static final String batchId = UUID.randomUUID().toString();
-
-
-    private static EyesRunner classicRunner = new ClassicRunner();
     private static BatchInfo batchInfo = new BatchInfo(JarArgsHandler.getBatchName());
     private static final LogHandler logHandler = new StdoutLogHandler(false);
 
     @Test
-    public void test1() throws Exception {
-        CbTestNGListener.startStep("Init");
-        System.out.println("Batch ID: " + batchId);
+    public void vgRunner() throws Exception {
+        CbTestNGListener.startStep("Setup driver");
+        System.out.println("This is using the VG");
         batchInfo.setId(batchId);
         String url = JarArgsHandler.getWebsite();
-        Eyes eyes = new Eyes(classicRunner);
+        Eyes eyes = new Eyes(new VisualGridRunner());
         eyes.setLogHandler(logHandler);
         WebDriver driver = CbTestNGListener.createWebDriver(true);
         CbTestNGListener.endLastStep();
-        //setupWebDriver();
         try {
-            CbTestNGListener.startStep("Open website");
+            CbTestNGListener.startStep("Open page");
             driver.get(url);
             eyes.setConfiguration(getConfiguration(eyes));
             driver = CbTestNGListener.wrapWebDriver(eyes.open(driver, appName, testName));
             driver.manage().window().maximize();
             CbTestNGListener.endLastStep();
-            CbTestNGListener.step("Validate", () -> {
+            CbTestNGListener.step("Check window", () -> {
                 eyes.check(Target.window().fully(true).withName(url));
                 eyes.closeAsync();
             });
-            //endStep("Step");
         } catch (Throwable t) {
             System.out.println("ERROR: session ID: " + ((RemoteWebDriver) driver).getSessionId());
             throw t;
@@ -62,48 +58,26 @@ public class EGTests { //extends CbTestNg {
     }
 
     @Test
-    public void test2() throws MalformedURLException {
-        CbTestNGListener.startStep("Init");
-        System.out.println("Batch ID: " + batchId);
+    public void classicRunner() throws MalformedURLException {
+        CbTestNGListener.startStep("Setup driver");
+        System.out.println("This is using the classic");
         batchInfo.setId(batchId);
         String url = JarArgsHandler.getWebsite();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        if (JarArgsHandler.isIsUsingTunnel() && JarArgsHandler.isIsUsingEgClient()) {
-            chromeOptions.setCapability("applitools:tunnel", true);
-        }
-        if (JarArgsHandler.isIsUsingTunnel() && !JarArgsHandler.isIsUsingEgClient()) {
-            chromeOptions.setCapability("browserName", "chrome");
-            chromeOptions.setCapability("applitools:x-tunnel-id-0", CbTestNGListener.getEnv("TUNNEL_ID"));
-            chromeOptions.setCapability("applitools:apiKey", CbTestNGListener.getEnv("APPLITOOLS_API_KEY"));
-            chromeOptions.setCapability("applitools:eyesServerUrl", CbTestNGListener.getEnv("APPLITOOLS_SERVER_URL"));
-            chromeOptions.addArguments("--headless");
-        }
-        WebDriver driver;
-        if (JarArgsHandler.isIsUsingEgClient()) {
-            driver = new RemoteWebDriver(new URL("http://localhost:8080/"), chromeOptions);
-        } else {
-            chromeOptions.setCapability("applitools:eyesServerUrl", CbTestNGListener.getEnv("APPLITOOLS_SERVER_URL"));
-            chromeOptions.setCapability("applitools:apiKey", CbTestNGListener.getEnv("APPLITOOLS_API_KEY"));
-            driver = new RemoteWebDriver(new URL("https://exec-wus.applitools.com/"), chromeOptions);
-        }
-        driver = CbTestNGListener.wrapWebDriver(driver);
-        //setupDriver(driver);
-        Eyes eyes = new Eyes(classicRunner);
+        Eyes eyes = new Eyes(new ClassicRunner());
         eyes.setLogHandler(logHandler);
+        WebDriver driver = CbTestNGListener.createWebDriver(true);
         CbTestNGListener.endLastStep();
         try {
-            //startStep("Step");
-            CbTestNGListener.startStep("Open website");
+            CbTestNGListener.startStep("Open page");
             driver.get(url);
             eyes.setConfiguration(getConfiguration(eyes));
-            driver = eyes.open(driver, appName, testName);
+            driver = CbTestNGListener.wrapWebDriver(eyes.open(driver, appName, testName));
             driver.manage().window().maximize();
             CbTestNGListener.endLastStep();
-            CbTestNGListener.step("Validate window", () -> {
+            CbTestNGListener.step("Check window", () -> {
                 eyes.check(Target.window().fully(true).withName(url));
+                eyes.closeAsync();
             });
-            eyes.closeAsync();
-            //endStep("Step");
         } catch (Throwable t) {
             System.out.println("ERROR: session ID: " + ((RemoteWebDriver) driver).getSessionId());
             throw t;
